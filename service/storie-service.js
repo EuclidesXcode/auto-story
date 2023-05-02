e6cec1974ab0002c9c61fb2810151904618decfa
EuclidesXcode <euclideslione@gmail.com>
2023-05-02 09:14:59 -0300

    change path

--- service/storie-service.js
+++ service/storie-service.js
@@ -7,7 +7,7 @@ class StoryService {
         
         console.log("entrou para criar o story")
 
-      const response = await Axios.post(`${process.env.BASE_PATH}/wp-json/wp/v2/web-story`, storyDto, {
+      const response = await Axios.post(`${process.env.BASE_PATH}/wp-json/wp/v1/web-story`, storyDto, {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${process.env.API_KEY_WP}`
